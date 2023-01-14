54e6bca8f7227ccb55163ea794408e1c445131f1
Andreas Laub <andreas@laub-home.de>
2023-01-14 13:43:11 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -29,11 +29,11 @@ if  sw.value():
         led.value(False)
     # Loading DHT22 Sensor Script
     #import dht22homiemqtt.py
-    #import dht22homiemqtt20.py
+    import dht22homiemqtt20.py
     # Loading BME680 Sensor Script
     #import bme680homiemqtt.py
     # Loading Pico Power Test Script
-    import pico_power_tests.py
+    #import pico_power_tests.py
 else:
     if config.ledstatus:
         blink = 5
